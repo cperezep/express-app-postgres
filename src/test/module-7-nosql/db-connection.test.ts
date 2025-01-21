3761d4af758bc4724d9e2bf82088ed48bc2ac96f
dianababurina-photobox <diana.baburina@photobox.com>
2025-01-21 19:41:09 +0200

    db connection fix

--- src/test/module-7-nosql/db-connection.test.ts
+++ src/test/module-7-nosql/db-connection.test.ts
@@ -1,27 +1,11 @@
 import mongoose from 'mongoose';
-
-export const DB_CONNECTION_STRING_AUTOCODE = 'mongodb://localhost:27017/admin';
-// export const DB_CONNECTION_STRING_LOCAL = 'mongodb://localhost:27017/mongo_db';
+import { DB_CONNECTION_STRING, DB_USER, DB_PASSWORD } from '../../env/db-connection';
 
 const COLLECTION_NAME: string = 'products';
 
-/*
-  If you want to run this test locally, make sure that
-  you are using DB_CONNECTION_STRING_LOCAL with valid credentials to the database.
-  If you are using the docker-compose.mongodb.yml file from this project,
-  simply use DB_CONNECTION_STRING_LOCAL and comment out DB_CONNECTION_STRING_AUTOCODE line.
-  While connecting to the database locally, you will need to provide user and password.
-  In this case please uncomment lines 24-27 below as well.
-*/
-
 describe('Database connection', () => {
   beforeAll(async () => {
-    await mongoose.connect(DB_CONNECTION_STRING_AUTOCODE, {
-      // For local development only
-      // user: 'mongo_user',
-      // pass: 'mongo_user_password',
-      // authSource: 'admin',
-    });
+    await mongoose.connect(DB_CONNECTION_STRING, (!DB_CONNECTION_STRING.includes('admin') ? { user: DB_USER, pass: DB_PASSWORD, authSource: 'admin' } : {}));
   });
 
   afterAll(async () => {
