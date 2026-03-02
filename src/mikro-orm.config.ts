aa66f0c5805e644e862ebde436c487dddcae8520
perezcr <cristian_perez@epam.com>
2026-03-02 16:59:13 -0500

    feat: update env variables

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,13 +2,13 @@ import type { Options } from '@mikro-orm/core';
 import { Migrator } from '@mikro-orm/migrations';
 import { PostgreSqlDriver } from '@mikro-orm/postgresql';
 import { SeedManager } from '@mikro-orm/seeder';
-import { DB_HOST, DB_PASSWORD, DB_PORT, DB_USER } from './env/postgresql-connection';
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from './env/postgresql-connection';
 import { Product } from './product/product.entity';
 
 const mikroOrmConfig: Options<PostgreSqlDriver> = {
   driver: PostgreSqlDriver,
   entities: [Product],
-  dbName: DB_USER,
+  dbName: DB_NAME,
   host: DB_HOST,
   port: Number(DB_PORT ?? 5432),
   user: DB_USER,
